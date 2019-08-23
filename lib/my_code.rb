6bfb52b28cfe7d0e78ee415f3c1bb26699bec1e0
Milen Khanal <milenkhanal@gmail.com>
2019-08-23 16:17:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -64,15 +64,7 @@ def reduce_to_all_true (arr, start = true)
  result
  end
   
-  def reduce_to_all_true (arr, start = false)
-    result = start 
-   i = 0 
-   while i < arr.length
-   result == arr[i]
-   i += 1 
-  end
- result
- end
+
   
   
 # def map_to_negativize (array)
